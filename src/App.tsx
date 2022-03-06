e87e627260f8d31ddac9df21ddc7739830c36645
taku1219 <acmiran189926@gmail.com>
2022-03-06 15:26:46 +0900

    [add]非同期

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,14 @@ import {
   incremented,
   resetCounter,
 } from './features/counter/counterSlice';
+import { useFetchBreedsQuery } from './features/dogs/dogsApiSlice';
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState<number>(10);
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   function handleIncremented() {
     dispatch(incremented());
@@ -37,6 +41,40 @@ function App() {
           <button onClick={handleDecremented}>decremented</button>
           <button onClick={handleReset}>resetCounter</button>
         </p>
+
+        <div>
+          <p>Dogs to fetch:</p>
+          <select
+            value={numDogs}
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+          >
+            <option value='5'>5</option>
+            <option value='10'>10</option>
+            <option value='15'>15</option>
+            <option value='20'>20</option>
+          </select>
+        </div>
+        <div>
+          <p>Number of dogs fetched:{data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,17 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/dogs/dogsApiSlice';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
